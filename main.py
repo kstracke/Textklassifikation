fba1cf7b707e000148f270fc03c64c85a1201e16
Katharina Stracke <kstracke@smail.uni-koeln.de>
2017-02-26 21:58:07 +0100

    Lagere in Funktionen aus

--- main.py
+++ main.py
@@ -14,37 +14,44 @@ pp = pprint.PrettyPrinter(indent=3)
 
 # Länge=Wortanzahl ausgeben
 
-learning_data = textimport.load_learning_data_from_file("Feedliste.txt")
+def process_tagged_urls(learning_data):
+    log = open("log.txt",mode="w")
 
-log = open("log.txt",mode="w")
+    per_subject_word_freq = {}
 
-per_subject_word_freq = {}
+    for subject in learning_data:
+        for url in learning_data[subject]:
+            RAW_TEXT = textimport.load_text_from_url(url)
 
-for subject in learning_data:
-    for url in learning_data[subject]:
-        RAW_TEXT = textimport.load_text_from_url(url)
+            log.write("***\n")
+            log.write(url + "\n")
+            log.write(RAW_TEXT + "\n")
 
-        log.write("***\n")
-        log.write(url + "\n")
-        log.write(RAW_TEXT + "\n")
+            if len(RAW_TEXT) > 0:
+                freq = textverarbeitung.makeWordFrequencyDictionary(RAW_TEXT)
+                pprint.pprint(freq, log)
 
-        if len(RAW_TEXT) > 0:
-            freq = textverarbeitung.makeWordFrequencyDictionary(RAW_TEXT)
-            pprint.pprint(freq, log)
+                per_subject_word_freq[subject] = textverarbeitung.appendWordFreqDictToExistingDict(
+                    per_subject_word_freq.get(subject, dict()), freq)
+            else:
+                log.write("ERROR: No text from this URL\n")
 
-            per_subject_word_freq[subject] = textverarbeitung.appendWordFreqDictToExistingDict(
-                per_subject_word_freq.get(subject, dict()), freq)
-        else:
-            log.write("ERROR: No text from this URL\n")
+            log.flush()
+            log.write("\n")
 
-        log.flush()
-        log.write("\n")
+    return per_subject_word_freq
 
-# gebe alle Wortfreq-Wörterbücher in Datei aus
-wordfreq_dict_file = open("wordlists.obj", mode="wb")
-pickle.dump(per_subject_word_freq, wordfreq_dict_file)
+def write_learning_data_to_file(per_subject_word_freq, filename):
+    # gebe alle Wortfreq-Wörterbücher in Datei aus
+
+    wordfreq_dict_file = open(filename, mode="wb")
+    pickle.dump(per_subject_word_freq, wordfreq_dict_file)
 
 # pprint.pprint(per_subject_word_freq, log)
+
+learning_data = textimport.load_learning_data_from_file("Feedliste.txt")
+per_subject_word_freq = process_tagged_urls(learning_data)
+write_learning_data_to_file(per_subject_word_freq, "wordlists.obj")
 
 log.write("\n\nResult:\n")
 for subject, wordfreq_dist in per_subject_word_freq.items():
